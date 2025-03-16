9f72a2ec4084b625383f35c47759ed3c3a5ccb9b
Vorkytaka <vorkytaka@gmail.com>
2025-03-16 20:21:55 +0300

    #8: flatMap

--- lib/src/transformation.dart
+++ lib/src/transformation.dart
@@ -17,6 +17,19 @@ extension ResultTransformation<T> on Result<T> {
   Result<R> mapCatching<R>(Transformer<T, R> transform) => isSuccess
       ? runCatching(() => transform(_value as T))
       : Result<R>._(_value);
+
+  /// Transforms a successful value to another [Result] using [transform].
+  ///
+  /// If successful, applies [transform] and returns its result.
+  /// If failed, returns the original failure.
+  @pragma('vm:prefer-inline')
+  Result<R> flatMap<R>(Result<R> Function(T value) transform) =>
+      _value is _Failure
+          ? Result<R>.failure(
+              _value.exception,
+              _value.stacktrace,
+            )
+          : transform(_value as T);
 }
 
 extension FutureResultTransformation<T> on Future<Result<T>> {
@@ -34,4 +47,12 @@ extension FutureResultTransformation<T> on Future<Result<T>> {
   @pragma('vm:prefer-inline')
   Future<Result<R>> mapCatching<R>(Transformer<T, R> transform) =>
       then((result) => result.mapCatching(transform));
+
+  /// Transforms a successful value to another [Result] using [transform].
+  ///
+  /// If successful, applies [transform] and returns its result.
+  /// If failed, returns the original failure.
+  @pragma('vm:prefer-inline')
+  Future<Result<R>> flatMap<R>(Result<R> Function(T value) transform) =>
+      then((result) => result.flatMap(transform));
 }
